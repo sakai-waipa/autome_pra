ef89219cd51db7d662199880a20e5388ad36f4f8
Sakai Yuki <ee174138@meiji.ac.jp>
2019-06-01 15:37:00 +0900

    change from int to double

--- shisoku.c
+++ shisoku.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 
 int main(){
-	int a, b;
+	double a, b;
 
 	a=2;
 	b=3;
 
-	printf("a+b=%d\n",a+b);
+	printf("a+b=%f\n",a+b);
 
-	printf("a-b=%d\n", a-b);
+	printf("a-b=%f\n", a-b);
 
-	printf("a*b=%d\n",a*b);
+	printf("a*b=%f\n",a*b);
 
-	printf("a/b=%d\n", a/b);
+	printf("a/b=%f\n", a/b);
 
 	return 0;
 }
